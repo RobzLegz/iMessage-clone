27277bd34bc59a2f295b954b6014d08b9248cba9
Robzlegz <robzlegz@gmail.com>
2021-01-30 19:00:27 +0200

    first bugs

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Chat.css";
 
 const Chat = () => {
+
+    const [input, setInput] = useState("");
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        setInput("");
+    }
+
     return (
         <div className="chat">
-            chat
+            <div className="chat-header">
+                <h4>To: <span className="roomName">Room Name</span></h4>
+                <strong>Details</strong>
+            </div>
+            <div className="chatInput">
+                <form>
+                    <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="iMessage"/>
+                    <button type="submit" onClick={sendMessage}>Send</button>
+                </form>
+            </div>
         </div>
     )
 }
