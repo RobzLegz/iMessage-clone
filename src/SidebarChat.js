59c4c193a4d695b41b3da428aad4caf780a2593a
Robzlegz <robzlegz@gmail.com>
2021-01-30 21:17:28 +0200

    adding timeago

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,13 +1,21 @@
 import { Avatar } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setChat } from './features/chatSlice';
+import db from './firebase';
 import "./SidebarChat.css";
+import * as timeago from 'timeago.js';
 
 const SidebarChat = ({id, chatName}) => {
 
     const dispatch = useDispatch();
-    
+    const [chatInfo, setChatInfo] = useState([]);
+
+    useEffect(() => {
+        db.collection("chats").doc(id).collection("messages").orderBy("timestamp", "desc").onSnapshot((snapshot) => (
+            setChatInfo(snapshot.docs.map((doc) => doc.data()))
+        ));
+    }, [id]);
 
     return (
         <div className="sidebarChat" onClick={() => 
@@ -21,8 +29,8 @@ const SidebarChat = ({id, chatName}) => {
             <Avatar />
             <div className="sidebarChat-info">
                 <h3>{chatName}</h3>
-                <p>Last message sent...</p>
-                <small>timestamp</small>
+                <p>{chatInfo[0]?.message}</p>
+                <small>{timeago.format(new Date(chatInfo[0]?.timestamp?.toDate()).toLocaleString())}</small>
             </div>
         </div>
     )
