511cd7cffff3e478c28f828ee7dd40814fab6440
Robzlegz <robzlegz@gmail.com>
2021-01-30 19:39:36 +0200

    setting up login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
+import { selectUser } from './features/userSlice';
+import { auth } from './firebase';
 import Imessage from './Imessage';
+import Login from './Login';
 
 function App() {
+
+  const user = useSelector(selectUser);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      if(authUser){
+        
+      }else{
+
+      }
+    })
+  }, [user])
+
   return (
     <div className="app">
-      <Imessage />
+      {user ? (
+        <Imessage />
+      ) : (
+        <Login />
+      )}
+      
     </div>
   );
 }
